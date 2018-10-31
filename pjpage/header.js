68c36179dbaaefca0533fade761a5915826ac958
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2018-10-31 17:36:05 +0900

    first commit

---
+++ pjpage/header.js
@@ -0,0 +1,6 @@
+document.write('\
+<div>\
+<a href="http://hirokatsukataoka.net/project/cc/index_cvpaperchallenge.html" target="_self"><img src="./img/cc_logo_2.png" style="margin-top: 5px;" height="100px" width="525px" /></a>\
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+</div>\
+');
