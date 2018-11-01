2e592d69960f9f3757ecf066f402a3d76f81a23f
Ryota Suzuki <suzuryo3893@gmail.com>
2018-11-01 18:13:39 +0900

    大きくスタイル調整 方針は自分でスペーシングしないスタイル 大きく自動マージン実装＆調整 code実装 tableスタイル実装

--- docs/footer.js
+++ docs/footer.js
@@ -1,5 +1,5 @@
 document.write('\
-<div style="text-align: center; width: 100%; margin-top: 25px; ">\
+<div class="footer" style="text-align: center; width: 100%; margin-top: 5em; margin-bottom: 1em;">\
 <p style="color: #999; font-size: 12pt; ">\
 &copy; <script>document.write(new Date().getFullYear())</script> cvpaper.challenge \
 </p></div>\
